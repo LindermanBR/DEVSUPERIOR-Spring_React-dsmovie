271a7bc02b23991cb256209bd7c8eb570ba7ddd7
LindermanBR <linderman.moura@outlook.com>
2022-05-12 20:43:47 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/requiemdev/dsmovie/entities/Score.java
@@ -0,0 +1,44 @@
+package com.requiemdev.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+	private Double score;
+	
+	public Score() {
+		
+	}
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+	
+	
+}
