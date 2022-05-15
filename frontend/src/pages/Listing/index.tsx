64abdb0ee81fbfdc1b350e59a18ef85a0e98c313
LindermanBR <linderman.moura@outlook.com>
2022-05-15 16:34:01 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { MoviePage } from "types/movie";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+import MovieCard from "components/MovieCard";
+import Pagination from "components/Pagination";
 import { BASE_URL } from "utils/requests";
-import MovieCard from "../../components/MovieCard";
-import Pagination from "../../components/Pagination";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
@@ -18,30 +18,25 @@ function Listing() {
         number: 0,
         first: true,
         numberOfElements: 0,
-        empty: true,
+        empty: true
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}$sort=id`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
             .then(response => {
                 const data = response.data as MoviePage;
                 setPage(data);
-
             });
-
     }, [pageNumber]);
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
+
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
@@ -52,10 +47,7 @@ function Listing() {
                     )}
                 </div>
             </div>
-
-
         </>
-
     );
 }
 
